37397d751fa42ea7cbcf411a1b9ae6cbe84f5ddb
DemonExposer <kbdmns@gmail.com>
2026-02-27 19:14:19 +0200

    Refactoring

--- Nockx.Base/C++/library.cpp
+++ Nockx.Base/C++/library.cpp
@@ -49,8 +49,8 @@ unsigned char generate_ml_kem_key() {
 	return 1;
 }
 
-unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *kem_key, const unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length) {
-	EVP_PKEY *parsed_key = d2i_PUBKEY(nullptr, &kem_key, kem_key_size);
+unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *public_kem_key, const unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length) {
+	EVP_PKEY *parsed_key = d2i_PUBKEY(nullptr, &public_kem_key, kem_key_size);
 	if (!parsed_key) {
 		fprintf(stderr, "Failed to parse key:\n");
 		ERR_print_errors_fp(stderr);
@@ -85,8 +85,8 @@ unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *kem_k
 	return 1;
 }
 
-unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, const unsigned int wrapped_encrypted_aes_key_length, const unsigned int shared_secret_length) {
-	EVP_PKEY *parsed_key = d2i_PUBKEY(nullptr, &kem_key, kem_key_size);
+unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *public_kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, const unsigned int wrapped_encrypted_aes_key_length, const unsigned int shared_secret_length) {
+	EVP_PKEY *parsed_key = d2i_PUBKEY(nullptr, &public_kem_key, kem_key_size);
 	if (!parsed_key) {
 		fprintf(stderr, "Failed to parse key:\n");
 		ERR_print_errors_fp(stderr);
@@ -145,8 +145,8 @@ unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *kem_key, unsigned
 	return 1;
 }
 
-unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *kem_key, unsigned int kem_key_size, const unsigned char *ciphertext, unsigned int ciphertext_length, unsigned char *decrypted_aes_key) {
-	EVP_PKEY *parsed_key = d2i_PrivateKey(OBJ_txt2nid("ML-KEM-768"), nullptr, &kem_key, kem_key_size);
+unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *private_kem_key, const unsigned int kem_key_size, const unsigned char *ciphertext, const unsigned int ciphertext_length, unsigned char *decrypted_aes_key) {
+	EVP_PKEY *parsed_key = d2i_PrivateKey(OBJ_txt2nid("ML-KEM-768"), nullptr, &private_kem_key, kem_key_size);
 	if (!parsed_key) {
 		fprintf(stderr, "Failed to parse key:\n");
 		ERR_print_errors_fp(stderr);
--- Nockx.Base/C++/library.h
+++ Nockx.Base/C++/library.h
@@ -3,10 +3,10 @@
 
 extern "C" unsigned char generate_ml_kem_key();
 
-extern "C" unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *kem_key, unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length);
+extern "C" unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *public_kem_key, unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length);
 
-extern "C" unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, unsigned int wrapped_encrypted_aes_key_length, unsigned int shared_secret_length);
+extern "C" unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *public_kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, unsigned int wrapped_encrypted_aes_key_length, unsigned int shared_secret_length);
 
-extern "C" unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *kem_key, unsigned int kem_key_size, const unsigned char *ciphertext, unsigned int ciphertext_length, unsigned char *decrypted_aes_key);
+extern "C" unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *private_kem_key, unsigned int kem_key_size, const unsigned char *ciphertext, unsigned int ciphertext_length, unsigned char *decrypted_aes_key);
 
 #endif // NOCKX_BASE_CPP_LIBRARY_H

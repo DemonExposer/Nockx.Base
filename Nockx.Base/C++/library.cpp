625564ba8c2174a2e6040777864d7cf9fb0ece6a
DemonExposer <kbdmns@gmail.com>
2026-02-28 03:23:19 +0200

    Load key from file and sign and verify

--- Nockx.Base/C++/library.cpp
+++ Nockx.Base/C++/library.cpp
@@ -5,25 +5,27 @@
 #include <openssl/err.h>
 #include <openssl/pem.h>
 
-unsigned char generate_ml_kem_key() {
+unsigned char generate_key(const char *key_type) {
 	OpenSSL_add_all_algorithms();
 	ERR_load_crypto_strings();
 
-	EVP_PKEY *kem_key = EVP_PKEY_Q_keygen(nullptr, nullptr, "ML-KEM-768");
+	EVP_PKEY *key = EVP_PKEY_Q_keygen(nullptr, nullptr, key_type);
 
-	if (!kem_key) {
+	if (!key) {
 		fprintf(stderr, "Error generating key:\n");
 		ERR_print_errors_fp(stderr);
 		return 0;
 	}
 
-	FILE *file = fopen("ml_kem_private_key.pem", "w");
+	std::string key_type_str(key_type);
+	std::ranges::transform(key_type_str, key_type_str.begin(), tolower);
+	FILE *file = fopen((key_type_str + std::string("_private_key.pem")).c_str(), "w");
 	if (!file) {
 		perror("fopen");
 		return 0;
 	}
 
-	if (!PEM_write_PrivateKey(file, kem_key, nullptr, nullptr, 0, nullptr, nullptr)) {
+	if (!PEM_write_PrivateKey(file, key, nullptr, nullptr, 0, nullptr, nullptr)) {
 		fprintf(stderr, "Error writing private key:\n");
 		ERR_print_errors_fp(stderr);
 		fclose(file);
@@ -31,13 +33,13 @@ unsigned char generate_ml_kem_key() {
 	}
 	fclose(file);
 
-	file = fopen("ml_kem_public_key.pem", "w");
+	file = fopen((key_type_str + std::string("_private_key.pem")).c_str(), "w");
 	if (!file) {
 		perror("fopen");
 		return 0;
 	}
 
-	if (!PEM_write_PUBKEY(file, kem_key)) {
+	if (!PEM_write_PUBKEY(file, key)) {
 		fprintf(stderr, "Error writing public key:\n");
 		ERR_print_errors_fp(stderr);
 		fclose(file);
@@ -45,7 +47,55 @@ unsigned char generate_ml_kem_key() {
 	}
 	fclose(file);
 
-	EVP_PKEY_free(kem_key);
+	EVP_PKEY_free(key);
+	return 1;
+}
+
+unsigned char get_key_sizes_from_file(const char *file_name, int *key_type_name_size, int *public_key_size, int *private_key_size) {
+	FILE *file = fopen(file_name, "r");
+	if (!file) {
+		perror("fopen");
+		return 0;
+	}
+
+	EVP_PKEY *key = PEM_read_PrivateKey(file, nullptr, nullptr, nullptr);
+	fclose(file);
+	if (!key) {
+		fprintf(stderr, "Error reading key:\n");
+		ERR_print_errors_fp(stderr);
+		return 0;
+	}
+
+	*private_key_size = i2d_PrivateKey(key, nullptr);
+	*public_key_size = i2d_PUBKEY(key, nullptr);
+	*key_type_name_size = static_cast<int>(strlen(EVP_PKEY_get0_type_name(key)));
+	EVP_PKEY_free(key);
+
+	return 1;
+}
+
+unsigned char read_key_from_file(const char *file_name, char *key_type, unsigned char *public_key, unsigned char *private_key) {
+	FILE *file = fopen(file_name, "r");
+	if (!file) {
+		perror("fopen");
+		return 0;
+	}
+
+	EVP_PKEY *key = PEM_read_PrivateKey(file, nullptr, nullptr, nullptr);
+	fclose(file);
+	if (!key) {
+		fprintf(stderr, "Error reading key:\n");
+		ERR_print_errors_fp(stderr);
+		return 0;
+	}
+
+	const char *name = EVP_PKEY_get0_type_name(key);
+	strcpy(key_type, name);
+
+	i2d_PrivateKey(key, &private_key);
+	i2d_PUBKEY(key, &public_key);
+	EVP_PKEY_free(key);
+
 	return 1;
 }
 
@@ -196,4 +246,115 @@ unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *private_kem_key, 
 		decrypted_aes_key[i] = shared_secret[i] ^ encrypted_aes_key[i];
 
 	return 1;
+}
+
+unsigned char get_signature_size(const unsigned char *private_key, const int key_size, const unsigned char *data, unsigned long data_size, unsigned int *signature_size) {
+	EVP_PKEY *parsed_key = d2i_PrivateKey(OBJ_txt2nid("ML-DSA-65"), nullptr, &private_key, key_size);
+	if (!parsed_key) {
+		fprintf(stderr, "Failed to parse key:\n");
+		ERR_print_errors_fp(stderr);
+		return 0;
+	}
+
+	EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+	if (!ctx) {
+		fprintf(stderr, "Failed to create context:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	if (EVP_DigestSignInit(ctx, nullptr, nullptr, nullptr, parsed_key) <= 0) {
+		fprintf(stderr, "Failed to initialize signing:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_MD_CTX_free(ctx);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	size_t sig_size;
+	if (EVP_DigestSign(ctx, nullptr, &sig_size, data, data_size) <= 0) {
+		fprintf(stderr, "Failed to sign (size check):\n");
+		ERR_print_errors_fp(stderr);
+		EVP_MD_CTX_free(ctx);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	*signature_size = sig_size;
+
+	EVP_MD_CTX_free(ctx);
+	EVP_PKEY_free(parsed_key);
+
+	return 1;
+}
+
+unsigned char sign_with_ml_dsa(const unsigned char *private_key, const int key_size, const unsigned char *data, const unsigned long data_size, unsigned char *signature, unsigned int *signature_size) {
+	EVP_PKEY *parsed_key = d2i_PrivateKey(OBJ_txt2nid("ML-DSA-65"), nullptr, &private_key, key_size);
+	if (!parsed_key) {
+		fprintf(stderr, "Failed to parse key:\n");
+		ERR_print_errors_fp(stderr);
+		return 0;
+	}
+
+	EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+	if (!ctx) {
+		fprintf(stderr, "Failed to create context:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	if (EVP_DigestSignInit(ctx, nullptr, nullptr, nullptr, parsed_key) <= 0) {
+		fprintf(stderr, "Failed to initialize signing:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_MD_CTX_free(ctx);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	if (EVP_DigestSign(ctx, signature, reinterpret_cast<size_t *>(signature_size), data, data_size) <= 0) {
+		fprintf(stderr, "Failed to sign:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_MD_CTX_free(ctx);
+		EVP_PKEY_free(parsed_key);
+		return 0;
+	}
+
+	EVP_MD_CTX_free(ctx);
+	EVP_PKEY_free(parsed_key);
+
+	return 1;
+}
+
+int verify_with_ml_dsa(const unsigned char *public_key, const int key_size, const unsigned char *data, unsigned long data_size, unsigned char *signature, unsigned int signature_size) {
+	EVP_PKEY *parsed_key = d2i_PUBKEY(nullptr, &public_key, key_size);
+	if (!parsed_key) {
+		fprintf(stderr, "Failed to parse key:\n");
+		ERR_print_errors_fp(stderr);
+		return -1;
+	}
+
+	EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+	if (!ctx) {
+		fprintf(stderr, "Failed to create context:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_PKEY_free(parsed_key);
+		return -1;
+	}
+
+	if (EVP_DigestVerifyInit(ctx, nullptr, nullptr, nullptr, parsed_key) <= 0) {
+		fprintf(stderr, "Failed to initialize verifying:\n");
+		ERR_print_errors_fp(stderr);
+		EVP_MD_CTX_free(ctx);
+		EVP_PKEY_free(parsed_key);
+		return -1;
+	}
+
+	int result = EVP_DigestVerify(ctx, signature, signature_size, data, data_size);
+
+	EVP_MD_CTX_free(ctx);
+	EVP_PKEY_free(parsed_key);
+
+	return result;
 }
--- Nockx.Base/C++/library.h
+++ Nockx.Base/C++/library.h
@@ -1,12 +1,24 @@
 #ifndef NOCKX_BASE_CPP_LIBRARY_H
 #define NOCKX_BASE_CPP_LIBRARY_H
 
-extern "C" unsigned char generate_ml_kem_key();
+extern "C" {
+	unsigned char generate_key(const char *key_type);
 
-extern "C" unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *public_kem_key, unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length);
+	unsigned char get_key_sizes_from_file(const char *file_name, int *key_type_name_size, int *public_key_size, int *private_key_size);
 
-extern "C" unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *public_kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, unsigned int wrapped_encrypted_aes_key_length, unsigned int shared_secret_length);
+	unsigned char read_key_from_file(const char *file_name, char *key_type, unsigned char *public_key, unsigned char *private_key);
 
-extern "C" unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *private_kem_key, unsigned int kem_key_size, const unsigned char *ciphertext, unsigned int ciphertext_length, unsigned char *decrypted_aes_key);
+	unsigned char get_ciphertext_and_shared_secret_length(const unsigned char *public_kem_key, unsigned int kem_key_size, unsigned int *ciphertext_length, unsigned int *shared_secret_length);
+
+	unsigned char encrypt_aes_key_with_ml_kem(const unsigned char *public_kem_key, unsigned int kem_key_size, const unsigned char *aes_key, unsigned char *wrapped_encrypted_aes_key, unsigned int wrapped_encrypted_aes_key_length, unsigned int shared_secret_length);
+
+	unsigned char decrypt_aes_key_with_ml_kem(const unsigned char *private_kem_key, unsigned int kem_key_size, const unsigned char *ciphertext, unsigned int ciphertext_length, unsigned char *decrypted_aes_key);
+
+	unsigned char get_signature_size(const unsigned char *private_key, int key_size, const unsigned char *data, unsigned long data_size, unsigned int *signature_size);
+
+	unsigned char sign_with_ml_dsa(const unsigned char *private_key, int key_size, const unsigned char *data, unsigned long data_size, unsigned char *signature, unsigned int *signature_size);
+
+	int verify_with_ml_dsa(const unsigned char *public_key, int key_size, const unsigned char *data, unsigned long data_size, unsigned char *signature, unsigned int signature_size);
+}
 
 #endif // NOCKX_BASE_CPP_LIBRARY_H
